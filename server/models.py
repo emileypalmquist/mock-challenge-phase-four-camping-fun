4903d210c7cefe0a16779481d1dbdb892021bef1
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2023-03-17 22:23:10 -0700

    added validates and association_proxy imports to models.py

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
 
